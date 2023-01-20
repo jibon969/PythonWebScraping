c0954e05a320d17e0b28246ceb0dd5827578e468
jayed969 <jayed.swe@gmail.com>
2023-01-20 22:41:43 +0600

    remove some code

--- BeautifulSoup4/00-Scraping_Final.py
+++ BeautifulSoup4/00-Scraping_Final.py
@@ -114,21 +114,21 @@ from bs4 import BeautifulSoup
 # print(image_data)
 
 
-# Make a request
-# page = requests.get("https://shop.shajgoj.com/")
-# soup = BeautifulSoup(page.content, 'html.parser')
-#
-# # Create top_items as empty list
-# all_links = []
-#
-# # Extract and store in top_items according to instructions on the left
-# links = soup.select('a')
-# for ahref in links:
-#     text = ahref.text
-#     text = text.strip() if text is not None else ''
-#
-#     href = ahref.get('href')
-#     href = href.strip() if href is not None else ''
-#     all_links.append({"href": href, "text": text})
-#
-# print(all_links)
+# Extracting Links
+page = requests.get("https://shop.shajgoj.com/")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# Create top_items as empty list
+all_links = []
+
+# Extract and store in top_items according to instructions on the left
+links = soup.select('a')
+for ahref in links:
+    text = ahref.text
+    text = text.strip() if text is not None else ''
+
+    href = ahref.get('href')
+    href = href.strip() if href is not None else ''
+    all_links.append({"href": href, "text": text})
+
+print(all_links)
